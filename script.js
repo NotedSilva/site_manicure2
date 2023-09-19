c3df27932d3bedb76adef178950c2575bdeeeb63
Gabriel S. Jesus <gabriel_live12@outlook.com>
2023-09-19 17:30:21 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let show = true;
+const MenuContent = document.querySelector('.content');
+const MenuToggle = MenuContent.querySelector('.menu-toggle');
+
+    MenuToggle.addEventListener('click', () => {
+    MenuContent.classList.toggle('on', show);
+    show = !show;
+});
